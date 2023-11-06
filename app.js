61d7f92613fe3f065e331e4494044a2f8b16d084
Muhammad Asif1163 <bibbasheikh11334@gmail.com>
2023-11-07 00:25:36 +0500

    2 file create

---
+++ app.js
@@ -0,0 +1,81 @@
+
+
+
+
+//Sum
+
+var num1 = 5;
+var num2 = 3;
+alert(num1 + num2)
+
+
+
+//subtraction,
+
+var num1 = 5;
+var num2 = 3;
+alert(num1 - num2)
+
+
+//multiplication,
+
+var num1 = 5;
+var num2 = 3;
+alert(num1 * num2)
+
+
+//division & modulus
+
+var num1 = 5;
+var num2 = 3;
+alert(num1 / num2)
+
+
+
+var num1 = 6;
+var num2 = 10;
+ 
+//ans is 16
+
+alert(num1 + num2)
+
+
+var num1 = 4;
+var num2 = 13;
+ 
+//ans is -9
+
+alert(num1 - num2)
+
+
+var num1 = 6;
+var num2 = 10;
+ 
+//ans is 55
+
+alert(num1 * num2);
+
+
+
+var num1 = 9;
+var num2 = 3;
+ 
+//ans is 0
+
+alert(num1 % num2)
+
+
+var num1 = "4";
+var num2 = 7;
+
+//ans is 47
+
+alert(num1 + num2)
+
+
+var num1 = "4";
+var num2 = 7;
+
+//ans is 42
+
+alert(num1 * num2)
